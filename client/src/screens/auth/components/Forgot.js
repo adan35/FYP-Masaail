3d452989de538cc088851c73055d022542f2e982
ammarshaiq004 <ammarshaiq004@gmail.com>
2021-11-29 20:37:20 +0500

    Forgot Page Frontend

--- client/src/screens/auth/components/Forgot.js
+++ client/src/screens/auth/components/Forgot.js
@@ -3,7 +3,24 @@ import React from "react";
 const Forgot = () => {
   return (
     <>
-     
+      <div className="container">
+        <div className="row card">
+          <div className="col text-center">
+            <h2>Enter Your Email</h2>
+            <form action="./otp">
+            <span>
+              <input
+                className="input-email"
+                id="email"
+                type="text"
+                placeholder="masail@gmail.com"
+              />
+            </span>
+              <button className="btn-send">Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
