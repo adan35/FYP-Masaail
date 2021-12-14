091b155de397dc000395f0c4aa11863f28034692
adan35 <adanshahzad35@gmail.com>
2021-12-14 17:47:00 +0500

    changing color from purple to cobalt blue

--- client/src/screens/auth/components/OTP.js
+++ client/src/screens/auth/components/OTP.js
@@ -1,62 +1,62 @@
 import React, { useState } from "react";
 
 const OTP = () => {
-  const [otp, setOtp] = useState(new Array(4).fill(""));
+    const [otp, setOtp] = useState(new Array(4).fill(""));
 
-  const handleChange = (element, index) => {
-    if (isNaN(element.value)) return false;
+    const handleChange = (element, index) => {
+        if (isNaN(element.value)) return false;
 
-    setOtp([...otp.map((d, idx) => (idx === index ? element.value : d))]);
+        setOtp([...otp.map((d, idx) => (idx === index ? element.value : d))]);
 
-    //Focus next input
-    if (element.nextSibling) {
-      element.nextSibling.focus();
-    }
-  };
+        //Focus next input
+        if (element.nextSibling) {
+            element.nextSibling.focus();
+        }
+    };
 
-  return (
-    <>
-      <div className="container">
-        <div className="row card">
-          <div className="col text-center">
-            <h2>Verify OTP</h2>
-            <p>Enter the OTP sent to you to verify your identity</p>
+    return (
+        <>
+            <div className="container">
+                <div className="row card">
+                    <div className="col text-center">
+                        <h2>Verify OTP</h2>
+                        <p>Enter the OTP sent to you to verify your identity</p>
 
-            {otp.map((data, index) => {
-              return (
-                <input
-                  className="otp-field"
-                  type="text"
-                  name="otp"
-                  maxLength="1"
-                  key={index}
-                  value={data}
-                  onChange={(e) => handleChange(e.target, index)}
-                  onFocus={(e) => e.target.select()}
-                />
-              );
-            })}
+                        {otp.map((data, index) => {
+                            return (
+                                <input
+                                    className="otp-field"
+                                    type="text"
+                                    name="otp"
+                                    maxLength="1"
+                                    key={index}
+                                    value={data}
+                                    onChange={(e) => handleChange(e.target, index)}
+                                    onFocus={(e) => e.target.select()}
+                                />
+                            );
+                        })}
 
-            <p>OTP Entered - {otp.join("")}</p>
-            <p>
-              <button
-                className="buttonClear"
-                onClick={(e) => setOtp([...otp.map((v) => "")])}
-              >
-                Clear
-              </button>
-              <button
-                className="button"
-                onClick={(e) => alert("Entered OTP is " + otp.join(""))}
-              >
-                Verify OTP
-              </button>
-            </p>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+                        <p>OTP Entered - {otp.join("")}</p>
+                        <p>
+                            <button
+                                className="buttonClear"
+                                onClick={(e) => setOtp([...otp.map((v) => "")])}
+                            >
+                                Clear
+                            </button>
+                            <button
+                                className="button"
+                                onClick={(e) => alert("Entered OTP is " + otp.join(""))}
+                            >
+                                Verify OTP
+                            </button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default OTP;
