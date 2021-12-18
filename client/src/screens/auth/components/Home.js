a71b069ece1580a39ad671ed233d530f6b4c6427
sameerabdullah <sameer322269@gmail.com>
2021-12-18 17:39:51 +0500

    adding login and homepage

--- client/src/screens/auth/components/Home.js
+++ client/src/screens/auth/components/Home.js
@@ -1,11 +1,30 @@
 import React from 'react'
-
+import '../../../App.css';
 const Home = () => {
     return (
-        <div>
-            homepage
+        <div class="home_container">
+          
+          <header>
+            <div class="primary-color home_logo">
+              <h2>MASAAIL.com</h2>
+            </div>
+            <div class="home_text">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+            </div>
+          </header>
+    
+          <main class="main_body">
+            <h4 class="primary-color">Are You ?</h4>
+            <div class="buttons_container">
+              <button class="btn">Reviewer</button>
+              <button class="btn">Investor</button>
+            </div>
+            <p class="already_registered">Already Registered? <a href="#">Login</a></p>
+          </main>
+    
+    
         </div>
-    )
+      );
 }
 
 export default Home;
--- client/src/screens/auth/components/Login.js
+++ client/src/screens/auth/components/Login.js
@@ -1,11 +1,29 @@
 import React from 'react'
-
+import '../../../App.css';
 const Login = () => {
     return (
         <div>
-            login
+          <header>
+            <div class="logo">
+              <h3>MASAAIL.com</h3>
+            </div>
+            <div class="login_text">
+              <p>Login</p>
+            </div>
+          </header>
+    
+          <div class="main">
+            <p class="sign" align="center">Sign in</p>
+            <form class="form1">
+              <input class="un " type="text" align="center" placeholder="Username" />
+              <input class="pass" type="password" align="center" placeholder="Password" />
+              <a class="submit" align="center">Sign in</a>
+              <p class="forgot" align="center"><a href="#">Forgot Password?</a></p>
+              <p class="signup">Don't have an account? <a href="#">Signup</a></p>
+            </form>                      
+          </div>
         </div>
-    )
+      );
 }
 
 export default Login;
