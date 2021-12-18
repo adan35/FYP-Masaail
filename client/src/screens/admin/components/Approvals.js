1763a3621e6356940b28b0029e5dcc03b8351151
ammarshaiq004 <ammarshaiq004@gmail.com>
2021-12-18 21:42:35 +0500

    adding admin/dashboard & admin/approvals

--- client/src/screens/admin/components/Approvals.js
+++ client/src/screens/admin/components/Approvals.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
 
 const Approvals = () => {
-    return (
-        <div>
-            approvals
+  return (
+    <body className="Approvals">
+      <div>
+        <div className="container1">
+          <div className="listview1">
+            <table id="list-pending">
+              <tr>
+                <td>
+                  <div className="circle-list">
+                    <h5 className="idNumber">1</h5>
+                  </div>
+                </td>
+                <td>
+                  <h5 className="name">Muhammad Ghyas</h5>
+                </td>
+                <td>
+                  <h5 className="dsgn">Reviewer</h5>
+                </td>
+                <td>
+                  <h5 className="city">Lahore, Pakistan</h5>
+                </td>
+                <td>
+                  <a href="#" className="see-post">
+                    See Posting
+                  </a>
+                </td>
+                <td className="button-approve">
+                  <button className="approve-btn button">Approve</button>
+                  <button className="delete-btn button">Delete</button>
+                
+                </td>
+              </tr>
+            </table>
+          </div>
         </div>
-    )
-}
+      </div>
+    </body>
+  );
+};
 
 export default Approvals;
--- client/src/screens/admin/components/Dashboard.js
+++ client/src/screens/admin/components/Dashboard.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Dashboard = () => {
-    return (
-        <div>
-            dashboard
+  return (
+    <body className="dashboard">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3"></div>
+          <div className="col-md-6">
+            <div className="card admin-card main">
+              <h2 className="userHeading">Users</h2>
+              <button className="investor">Investor</button>
+              <button className="investor">Reviewer</button>
+              <button className="approvals-pending">Approvals Pending</button>
+            </div>
+          </div>
+          <div className="col-md-3"></div>
         </div>
-    )
-}
+      </div>
+    </body>
+  );
+};
 
 export default Dashboard;
