2d8701651f3783dc4e9cec2cc52a7ccce8261d5d
adan35 <adanshahzad35@gmail.com>
2022-01-02 10:34:08 -0800

    removing jsx warnings

--- client/src/screens/admin/components/Dashboard.js
+++ client/src/screens/admin/components/Dashboard.js
@@ -1,61 +1,13 @@
 import React from "react";
-import {
-  ProSidebar,
-  Menu,
-  MenuItem,
-  SidebarHeader,
-  SidebarFooter,
-  SidebarContent,
-} from "react-pro-sidebar";
-import { IoIosCreate } from "react-icons/io";
-import { MdTimeline, MdReviews, MdPending, MdHome } from "react-icons/md";
-import { FaQuestionCircle, FaMoneyBillWave } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import "react-pro-sidebar/dist/css/styles.css";
-import "../../react-pro-sidebar.css";
-
+import AdminSidebar from "./AdminSidebar";
 const Dashboard = () => {
   return (
     <>
       <div>
-        <ProSidebar className="pro-sidebar">
-          <SidebarHeader>
-        <h1>Masaail</h1>
-          </SidebarHeader>
-
-          <SidebarContent>
-            <Menu iconShape="square">
-            <MenuItem icon={<MdHome />}>
-                Dashboard
-                <Link to="#" />
-              </MenuItem>
-              <MenuItem icon={<MdPending />}>
-                Approvals
-                <Link to="/admin/approvals" />
-              </MenuItem>
-              <MenuItem icon={<MdReviews />}>
-                Reviewers
-                <Link to="/admin/reviewers" />
-              </MenuItem>
-
-              <MenuItem icon={<FaMoneyBillWave />}>
-                Investors
-                <Link to="/admin/investors" />
-              </MenuItem>
-
-              <MenuItem icon={<FaQuestionCircle />}>
-                Queries
-                <Link to="/admin/queries" />
-              </MenuItem>
-            </Menu>
-          </SidebarContent>
-
-          <SidebarFooter className="sidebar-footer">
-            <p className="para">
-              copyright@2022 <br /> Masaail.com
-            </p>
-          </SidebarFooter>
-        </ProSidebar>
+        
+        <AdminSidebar />
+        
         <div className="main-content">
             <div className="row">
           <div className="col-md-1"></div>
@@ -64,27 +16,28 @@ const Dashboard = () => {
               <div className="container">
                 <ul className="list-unstyled">
                   <li>
-                    <div class="container">
-                      <div class="card">
+                    <div className="container">
+                      <div className="card">
                         <img
                           src="https://cdn.pixabay.com/photo/2014/04/02/10/12/checkbox-303113_960_720.png"
-                          class="card-img-top img"
+                          className="card-img-top img"
+                          alt="img1"
                         />
 
-                        <div class="card-body">
+                        <div className="card-body">
                           <center>
-                            <h5 class="card-title">Approvals</h5>
+                            <h5 className="card-title">Approvals</h5>
                           </center>
 
-                          <p class="card-text">
+                          <p className="card-text">
                            50 Posts are pending for approvals.
                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown p
                           </p>
 
                           <center>
-                            <a href="/admin/approvals" class="btn button">
+                            <Link to="/admin/approvals" className="btn button">
                               Approve Posts
-                            </a>
+                            </Link>
                           </center>
                         </div>
                       </div>
@@ -98,27 +51,28 @@ const Dashboard = () => {
             <div className="container">
               <ul className="list-unstyled">
                 <li>
-                  <div class="container">
-                    <div class="card">
+                  <div className="container">
+                    <div className="card">
                       <img
                         src="https://cdn-icons-png.flaticon.com/512/942/942751.png"
-                        class="card-img-top img"
+                        className="card-img-top img"
+                        alt="img2"
                       />
 
-                      <div class="card-body">
+                      <div className="card-body">
                         <center>
-                          <h5 class="card-title">Queries</h5>
+                          <h5 className="card-title">Queries</h5>
                         </center>
 
-                        <p class="card-text">
+                        <p className="card-text">
                             50 queries are pending to be answered.
                         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer too
                         </p>
 
                         <center>
-                          <a href="/admin/chats" class="btn button">
+                          <Link to="/admin/queries" className="btn button">
                             Queries
-                          </a>
+                          </Link>
                         </center>
                       </div>
                     </div>
--- client/src/screens/admin/components/Reviewers.js
+++ client/src/screens/admin/components/Reviewers.js
@@ -1,61 +1,12 @@
 import React from "react";
-import {
-  ProSidebar,
-  Menu,
-  MenuItem,
-  SidebarHeader,
-  SidebarFooter,
-  SidebarContent,
-} from "react-pro-sidebar";
-import { IoIosCreate } from "react-icons/io";
-import { MdTimeline, MdReviews, MdPending, MdHome } from "react-icons/md";
-import { FaQuestionCircle, FaMoneyBillWave } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import "react-pro-sidebar/dist/css/styles.css";
-import "../../react-pro-sidebar.css";
+import AdminSidebar from "./AdminSidebar";
+
 const Reviewers = () => {
   return (
     <>
-        <ProSidebar className="pro-sidebar">
-          <SidebarHeader>
-            <h1>Masaail</h1>
-          </SidebarHeader>
-
-          <SidebarContent>
-            <Menu iconShape="square">
-              <MenuItem icon={<MdHome />}>
-                Dashboard
-                <Link to="/admin/" />
-              </MenuItem>
-              <MenuItem icon={<MdPending />}>
-                Approvals
-                <Link to="/admin/approvals" />
-              </MenuItem>
-              <MenuItem icon={<MdReviews />}>
-                Reviewers
-                <Link to="/admin/reviewers" />
-              </MenuItem>
-
-              <MenuItem icon={<FaMoneyBillWave />}>
-                Investors
-                <Link to="/admin/investors" />
-              </MenuItem>
-
-              <MenuItem icon={<FaQuestionCircle />}>
-                Queries
-                <Link to="/admin/queries" />
-              </MenuItem>
-            </Menu>
-          </SidebarContent>
-
-          <SidebarFooter className="sidebar-footer">
-            <p className="para">
-              copyright@2022 <br /> Masaail.com
-            </p>
-          </SidebarFooter>
-        </ProSidebar>
+        <AdminSidebar />
         <div className="main-content overflow-auto">
-        <table class="table table-hover">
+        <table className="table table-hover">
             <thead>
               <tr>
                 <td>#</td>
