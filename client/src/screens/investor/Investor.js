c5592a42e4c279a0b9a0fac67e37ee2bfa52dc64
Harssiii <m.harry390@gmail.com>
2022-01-02 01:06:20 -0800

    nav bar for investor

--- client/src/screens/investor/Investor.js
+++ client/src/screens/investor/Investor.js
@@ -5,6 +5,24 @@ import "./investor.css";
 const Investor = (params) => {
     return (
         <div>
+            <div className='row mt-4'>
+                <div className='col-md-2'>
+                    <div className='title'>
+                        <p className='text'>MASAIL.COM</p>
+                    </div>
+                </div>
+                <div className='col-md-3'>
+                    <div class="form-group has-search">
+                        <span class="fa fa-search form-control-feedback"></span>
+                        <input type="text" class="form-control" placeholder="Search"/>
+                    </div>
+                </div>
+                <div className='col'>
+                    <div className='text-end profile'>
+                        <i class="fas fa-sort-down"></i>
+                    </div>
+                </div>
+            </div>
             <Switch>
                 <Route exact path="/investor" component={Timeline} />
                 <Route exact path="/investor/chatbox" component={ChatBox} />
