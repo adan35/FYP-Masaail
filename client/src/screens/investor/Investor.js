6cffebb9a90cbff82bc3a8bb924101a2e7b88120
Harssiii <m.harry390@gmail.com>
2022-01-02 02:27:45 -0800

    profile img

--- client/src/screens/investor/Investor.js
+++ client/src/screens/investor/Investor.js
@@ -5,6 +5,7 @@ import "./investor.css";
 const Investor = (params) => {
     return (
         <div>
+            <div>
             <div className='row mt-4'>
                 <div className='col-md-2'>
                     <div className='title'>
@@ -23,36 +24,38 @@ const Investor = (params) => {
                     </div>
                 </div>
             </div>
+            </div>
             <div className='row mt-4'>
-            <div className='col-md-3'>
-                <button className='btn post-btn'><i class="fas fa-pen-square"></i>Create Post</button>
-                <button className='btn post-btn'><i class="fas fa-home"></i>Timeline</button>
-                <button className='btn post-btn'><i class="fas fa-clipboard-list"></i>My Reviews</button>
-            </div>
-            <div className='col'>
-                <Switch>
-                    <Route exact path="/investor" component={Timeline} />
-                    <Route exact path="/investor/chatbox" component={ChatBox} />
-                    <Route exact path="/investor/createpoll" component={CreatePoll} />
-                    <Route exact path="/investor/mypolls" component={MyPolls} />
-                </Switch>
-            </div>
-            <div className='col-md-3'>
-                <h4>Hot Topics</h4>
-                <div className='hot-topic'>
-                    <h4>Lorem Ipsum</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <div className='col-md-3'>
+                    <button className='btn post-btn'><i class="fas fa-pen-square"></i>Create Post</button>
+                    <button className='btn post-btn'><i class="fas fa-home"></i>Timeline</button>
+                    <button className='btn post-btn'><i class="fas fa-clipboard-list"></i>My Reviews</button>
                 </div>
-                <div className='hot-topic'>
-                    <h4>Lorem Ipsum</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <div className='col'>
+                    <Switch>
+                        <Route exact path="/investor" component={Timeline} />
+                        <Route exact path="/investor/chatbox" component={ChatBox} />
+                        <Route exact path="/investor/createpoll" component={CreatePoll} />
+                        <Route exact path="/investor/mypolls" component={MyPolls} />
+                    </Switch>
                 </div>
-                <div className='hot-topic'>
-                    <h4>Lorem Ipsum</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <div className='col-md-3'>
+                    <h4>Hot Topics</h4>
+                    <div className='hot-topic'>
+                        <h4>Lorem Ipsum</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                    <div className='hot-topic'>
+                        <h4>Lorem Ipsum</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                    <div className='hot-topic'>
+                        <h4>Lorem Ipsum</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
                 </div>
             </div>
-            </div>
+            
         </div>
     );
 }
--- client/src/screens/investor/components/Timeline.js
+++ client/src/screens/investor/components/Timeline.js
@@ -5,10 +5,146 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import img1 from "../../../assets/img/1.jpg";
 import img2 from "../../../assets/img/2.jpg";
 import img3 from "../../../assets/img/3.jpg";
+import user from "../../../assets/img/user.jpg";
 
 const Timeline = () => {
     return (
         <>
+            <div className='post'>
+                <div className='row'>
+                    <div className='col-md-2 profile-img'>
+                        <div className='img'>
+                            <img src={user} />
+                        </div>
+                    </div>
+                    <div className='col-md-4'>
+                        <h4>Muhammad Haris</h4>
+                        <h5>Lahore</h5>
+                    </div>
+                    <div className='col-md-6'>
+                        <h5 className='text-end'>231 Views</h5>
+                    </div>
+                </div>
+                <div className='row'>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc varius feugiat velit, at finibus leo dignissim ac. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis auctor condimentum tortor, et viverra turpis bibendum non. Cras id orci elementum, hendrerit ligula vel, pharetra enim. Vivamus metus sapien, cursus eget mollis eu, feugiat
+                    </p>
+                </div>
+                <div>
+                <Carousel>
+                    <div>
+                        <img src={img1} />
+                    </div>
+                    <div>
+                        <img src={img2} />
+                    </div>
+                    <div>
+                        <img src={img3} />
+                    </div>
+                </Carousel>
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                        <i class="fas fa-plus-circle"></i>
+                        <i class="fas fa-heart"></i>
+                    </div>
+                    <div className='col-md-4'></div>
+                    <div className='col-md-6'>
+                        <h5 className='text-end'>231 Support</h5>
+                    </div>
+                </div>
+            </div>
+            
+            <div className='post'>
+                <div className='row'>
+                    <div className='col-md-2 profile-img'>
+                        <div className='img'>
+
+                        </div>
+                    </div>
+                    <div className='col-md-4'>
+                        <h4>Muhammad Haris</h4>
+                        <h5>Lahore</h5>
+                    </div>
+                    <div className='col-md-6'>
+                        <h5 className='text-end'>231 Views</h5>
+                    </div>
+                </div>
+                <div className='row'>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc varius feugiat velit, at finibus leo dignissim ac. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis auctor condimentum tortor, et viverra turpis bibendum non. Cras id orci elementum, hendrerit ligula vel, pharetra enim. Vivamus metus sapien, cursus eget mollis eu, feugiat
+                    </p>
+                </div>
+                <div>
+                <Carousel>
+                    <div>
+                        <img src={img1} />
+                    </div>
+                    <div>
+                        <img src={img2} />
+                    </div>
+                    <div>
+                        <img src={img3} />
+                    </div>
+                </Carousel>
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                        <i class="fas fa-plus-circle"></i>
+                        <i class="fas fa-heart"></i>
+                    </div>
+                    <div className='col-md-4'></div>
+                    <div className='col-md-6'>
+                        <h5 className='text-end'>231 Support</h5>
+                    </div>
+                </div>
+            </div>
+
+            <div className='post'>
+                <div className='row'>
+                    <div className='col-md-2 profile-img'>
+                        <div className='img'>
+
+                        </div>
+                    </div>
+                    <div className='col-md-4'>
+                        <h4>Muhammad Haris</h4>
+                        <h5>Lahore</h5>
+                    </div>
+                    <div className='col-md-6'>
+                        <h5 className='text-end'>231 Views</h5>
+                    </div>
+                </div>
+                <div className='row'>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc varius feugiat velit, at finibus leo dignissim ac. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis auctor condimentum tortor, et viverra turpis bibendum non. Cras id orci elementum, hendrerit ligula vel, pharetra enim. Vivamus metus sapien, cursus eget mollis eu, feugiat
+                    </p>
+                </div>
+                <div>
+                <Carousel>
+                    <div>
+                        <img src={img1} />
+                    </div>
+                    <div>
+                        <img src={img2} />
+                    </div>
+                    <div>
+                        <img src={img3} />
+                    </div>
+                </Carousel>
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                        <i class="fas fa-plus-circle"></i>
+                        <i class="fas fa-heart"></i>
+                    </div>
+                    <div className='col-md-4'></div>
+                    <div className='col-md-6'>
+                        <h5 className='text-end'>231 Support</h5>
+                    </div>
+                </div>
+            </div>
+
             <div className='post'>
                 <div className='row'>
                     <div className='col-md-2 profile-img'>
